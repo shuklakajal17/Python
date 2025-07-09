043a7a6300d1ce2abe3e4dfcdd8929917a87a9c3
kajal shukla <shuklakajal1703@gmail.com>
2025-07-09 13:09:34 +0530

    Add files via upload

---
+++ forthProgram.py
@@ -0,0 +1,25 @@
+# type convertion 
+a = 2
+b = 4.25
+
+sum = a + b
+print(sum)
+
+# type casting
+
+a = int("2")
+b = 4.25
+
+print(type(a))
+print(a + b)
+
+
+# input
+
+name = input("enter your name:")
+age = int(input("enter age:"))
+marks = float(input("enter marks:"))
+
+print("welcome", name)
+print("age =", age)
+print("marks =", marks)
