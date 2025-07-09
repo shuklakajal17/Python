d7faebfd8053ca194220d6617d99bfac5b3a179b
kajal shukla <shuklakajal1703@gmail.com>
2025-07-09 13:09:21 +0530

    Add files via upload

---
+++ thirdProgram.py
@@ -0,0 +1,42 @@
+# arithmetic operators
+
+a = 2
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)    
+print(a % b)
+print(a ** b)
+
+
+
+# relational/ comparison operators
+
+a = 50
+b = 20
+
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a > b)
+
+
+# assingment operators
+
+num = 10
+num += 10
+print("num : ", num)
+
+
+# logical operators
+
+a = 50
+b = 30
+print(not False)
+print(not (a > b))
+
+val1 = True 
+val2 = True
+print("and operators:", val1 and val2) 
+print("or operators:", val1 or val2) 
