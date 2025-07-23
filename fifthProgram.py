dd01900ccd168d126096468fc6e2d1d3aff70267
kajal shukla <shuklakajal1703@gmail.com>
2025-07-23 12:59:56 +0530

    Add files via upload

---
+++ fifthProgram.py
@@ -0,0 +1,72 @@
+#concatenation
+
+str1 = "apna"
+str2 = "college"
+final_str = str1+str2 
+print(final_str)
+
+#length of str
+
+str1 = "apna"
+len1 = len(str1)
+print(len1)
+
+str2 = "college"
+len2 = len(str2)
+print(len2)
+
+final_str = str1+ " " + str2 
+print(final_str)
+print(len(final_str))
+
+#indexing
+
+str = "apna college"
+ch = str[1]
+print(ch)
+
+
+#slicing
+
+str = "kajal shukla"
+print(str[5:len(str)])
+
+
+#negative index
+
+str = "apple"
+print(str[-3:-1])
+
+
+#stirng function
+
+str = "I am studing python from ApnaCollege"
+print(str.endswith("ege"))
+
+
+#conditional statement
+
+light = "pink"
+
+if(light == "red"):
+    print("stop")
+elif(light == "green"):
+    print("go")
+elif(light == "yellow"):
+    print("look")
+else:
+    print("light is broken")
+
+    print("end of code")
+
+
+
+
+
+
+
+
+
+
+
+
