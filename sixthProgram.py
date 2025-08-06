9e5bd301aa324ae8ab0c62dfc85055336c7e6326
kajal shukla <shuklakajal1703@gmail.com>
2025-08-06 20:59:49 +0530

    Add files via upload

---
+++ sixthProgram.py
@@ -0,0 +1,20 @@
+#list in python
+
+marks = [94.4, 87.5, 95.2, 66.4, 45.1]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+print(len(marks))
+
+#list slicing
+
+marks = [85, 94, 76, 63, 48]
+print(marks[1:4])
+
+#list method
+
+list = [2, 1, 3]
+print(list.append(4))
+print(list.sort())
+print(list)
