f04d2ff113772da8531a27c8d53a27e554627b04
kajal shukla <shuklakajal1703@gmail.com>
2025-08-11 13:20:07 +0530

    Add files via upload

---
+++ practice-11.py
@@ -0,0 +1,9 @@
+movies = []
+mov = input("enter 1st movies: ")
+movies.append(mov)
+mov = input("enter 2nd movies: ")
+movies.append(mov)
+mov = input("enter 3rd movies: ")
+movies.append(mov)
+
+print(movies)
